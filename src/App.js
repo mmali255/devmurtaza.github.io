05f4165a9f0990083c9e6ba81a533bcda7165e70
murtaza ali <murtazamustafa255@gmail.com>
2022-12-22 11:48:09 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import Layout from "./Layout";
 import FaceMask from "./projects/ml/FaceMask";
+import ScrollToTop from "./utils/ScrollToTop";
 function App() {
   return (
     <React.Fragment>
-      <Routes>
-        <Route path="/projects/ml/0" element={<FaceMask />} />
-        <Route path="/*" element={<Layout />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/projects/ml/0" element={<FaceMask />} />
+
+          <Route path="/*" element={<Layout />} />
+        </Routes>
+      </ScrollToTop>
     </React.Fragment>
   );
 }
